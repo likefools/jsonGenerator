a1203d8e7c98d43356df9b2e09a92ec3422169f8
user <user@domain.com>
2021-10-22 13:27:05 +0300

    build

--- src/app/main-List/main-List.Rout.ts
+++ src/app/main-List/main-List.Rout.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { productList } from 'src/products';
 import { faCopy } from '@fortawesome/free-regular-svg-icons';
 import { ClipboardService } from 'ngx-clipboard';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'main-list-rout',
@@ -11,6 +12,7 @@ import { ClipboardService } from 'ngx-clipboard';
 export class mainListRout {
   constructor(private clip: ClipboardService) { }
   iconCopy = faCopy
+  iconAdd = faPlus
   products = productList
   copyObject() {
     this.clip.copy(JSON.stringify(productList))
